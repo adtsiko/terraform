3d4f5bd3d821f37c2b4686237a509444bd1a8371
adtsiko <anesutsiko@live.com>
2022-02-18 18:07:46 +0000

    main

--- main.tf
+++ main.tf
@@ -5,4 +5,8 @@ provider "aws" {
 resource "aws_instance" "my_instance" {
     ami = "ami-0dd555eb7eb3b7c82"
     instance_type = "t2.micro"  
+
+    tags = {
+        Name = "ec2-instance"
+    }
 }
